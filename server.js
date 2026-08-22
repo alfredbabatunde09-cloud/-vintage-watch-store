128ad315627b9e3f4397481b8bd8ef1f6328c7e7
alfredbabatunde09-cloud <alfredbabatunde09@gmail.com>
2026-08-22 02:29:54 +0100

    Change catch-all route to use splat parameter

--- server.js
+++ server.js
@@ -192,7 +192,7 @@ app.get("/admin", (req, res) => {
   res.sendFile(path.join(__dirname, "admin.html"));
 });
 
-app.get("*", (req, res) => {
+app.get("/{*splat}", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
